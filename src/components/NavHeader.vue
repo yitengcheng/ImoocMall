969517150664921b7734a7b4745ea8ba3048f871
fanzehua <316679581@qq.com>
2019-05-29 16:44:48 +0800

    修改logo图标

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -22,7 +22,7 @@
     <div class="navbar">
       <div class="navbar-left-container">
         <a href="/">
-          <img class="navbar-brand-logo" src="static/logo.jpg">
+          <img class="navbar-brand-logo" src="static/logo.png">
         </a>
       </div>
       <div class="navbar-right-container" style="display: flex;">
